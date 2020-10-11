25782ce6baa413a68192fe2b202bf044bf9fed7e
agtArcher <stranger9905@gmail.com>
2020-10-11 13:29:59 +0300

    added home page

--- src/main/java/com/example/projectmanagement/controllers/ProjectController.java
+++ src/main/java/com/example/projectmanagement/controllers/ProjectController.java
@@ -4,7 +4,6 @@ import com.example.projectmanagement.dao.ProjectDao;
 import com.example.projectmanagement.ds.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,9 +17,8 @@ public class ProjectController {
     private ProjectDao projectDao;
 
     @GetMapping("/new")
-    public String displayProjectForm(Model model, @ModelAttribute("project") Project project) {
-        model.addAttribute("projects", projectDao.findAll());
-        return "new-project";
+    public String displayProjectForm(@ModelAttribute("project") Project project) {
+        return "projects/new-project";
     }
 
     @PostMapping("/save")
