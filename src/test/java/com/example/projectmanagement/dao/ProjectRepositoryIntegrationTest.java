e372b9d1d5f4c47020a9871eef85fb9b91a2bb8c
agtArcher <stranger9905@gmail.com>
2020-10-13 11:49:10 +0300

    change annotation to springboottest

--- src/test/java/com/example/projectmanagement/dao/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/example/projectmanagement/dao/ProjectRepositoryIntegrationTest.java
@@ -7,14 +7,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@ContextConfiguration(classes = ProjectManagementApplication.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@DataJpaTest
 @SqlGroup({
         @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:schema.sql", "classpath:data.sql"}),
         @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = {"classpath:drop.sql"})
