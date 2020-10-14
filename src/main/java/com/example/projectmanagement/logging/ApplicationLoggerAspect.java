9b091c129fe3f6cb5f9c08ffcb65ea525ce503e7
agtArcher <stranger9905@gmail.com>
2020-10-14 12:38:08 +0300

    configured logging aspect

--- src/main/java/com/example/projectmanagement/logging/ApplicationLoggerAspect.java
+++ src/main/java/com/example/projectmanagement/logging/ApplicationLoggerAspect.java
@@ -1,12 +1,12 @@
 package com.example.projectmanagement.logging;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -19,8 +19,27 @@ public class ApplicationLoggerAspect {
         //empty method just to name location specified in the pointcut
     }
 
-    @Before("definePackagePointcuts()")
-    public void log() {
-        log.debug(" ---------------------------------------- ");
+    @Around("definePackagePointcuts()")
+    public Object logAround(ProceedingJoinPoint jp) {
+
+        log.debug(" \n \n \n ");
+
+        log.debug("************** Before Method Execution ************ \n " +
+                "{}.{} () with argument[s] = {}", jp.getSignature().getDeclaringTypeName(),
+                jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+        log.debug("___________________________________________________ \n \n \n");
+        Object o = null;
+        try {
+            o = jp.proceed();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
+        log.debug("************** After Method Execution ************ \n " +
+                        "{}.{} () with argument[s] = {}", jp.getSignature().getDeclaringTypeName(),
+                jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+        log.debug("___________________________________________________ \n \n \n");
+
+        return o;
     }
 }
