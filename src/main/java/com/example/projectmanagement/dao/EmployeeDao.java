78ad3d09efacb341b9fb0a4e80f4d28633ca5032
agtArcher <stranger9905@gmail.com>
2020-10-16 17:09:11 +0300

    plug in rest data starter

--- src/main/java/com/example/projectmanagement/dao/EmployeeDao.java
+++ src/main/java/com/example/projectmanagement/dao/EmployeeDao.java
@@ -4,9 +4,11 @@ import com.example.projectmanagement.ds.Employee;
 import com.example.projectmanagement.dto.EmployeeProject;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(collectionResourceRel = "apiemployees", path = "apiemployees")
 public interface EmployeeDao extends PagingAndSortingRepository<Employee, Long> {
 
     @Query(nativeQuery = true, value = "SELECT e.first_name as firstName, e.last_name as lastName, COUNT(pe.employee_id) as projectCount " +
--- src/main/java/com/example/projectmanagement/dao/ProjectDao.java
+++ src/main/java/com/example/projectmanagement/dao/ProjectDao.java
@@ -4,9 +4,11 @@ import com.example.projectmanagement.ds.Project;
 import com.example.projectmanagement.dto.ChartData;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "apiprojects", path = "apiprojects")
 public interface ProjectDao extends PagingAndSortingRepository<Project, Long> {
 
     @Override
